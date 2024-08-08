22510a94548d207188f70bf9cef6f698925708ca
wutong <wt3212834434@outlook.com>
2024-08-08 14:25:36 +0800

    弹出层搭建

--- pages/preview/components/wallpaper-info.vue
+++ pages/preview/components/wallpaper-info.vue
@@ -1,6 +1,6 @@
 <template>
   <view class="wallpaperInfo">
-    <view class="box">
+    <view class="box" @click="checkDetail">
       <uni-icons type="info" size="24"></uni-icons>
       <view class="text">信息</view>
     </view>
@@ -13,9 +13,68 @@
       <view class="text">下载</view>
     </view>
   </view>
+
+  <!--弹出层-->
+  <uni-popup ref="infoPopup" type="bottom">
+    <view class="info-popup">
+      <view class="pop-header">
+        <view></view>
+        <view class="title">壁纸信息</view>
+        <view class="close">
+          <uni-icons type="closeempty" size="18" color="#999" @click="closePopup"></uni-icons>
+        </view>
+      </view>
+
+      <scroll-view scroll-y="true">
+        <view class="content">
+          <view class="row">
+            <view class="label">壁纸ID：</view>
+            <text selectable="true" class="value">123123123123abcdefg</text>
+          </view>
+          <view class="row">
+            <view class="label">分类：</view>
+            <text selectable="true" class="value category">明星美女</text>
+          </view>
+          <view class="row">
+            <view class="label">发布者：</view>
+            <text selectable="true" class="value">咸虾米</text>
+          </view>
+          <view class="row">
+            <view class="label">评分：</view>
+            <view class="value rate-box">
+              <uni-rate readonly touchable value="3" size="16"></uni-rate>
+              <text class="score">5分</text>
+            </view>
+          </view>
+          <view class="row">
+            <view class="label">摘要：</view>
+            <text selectable="true" class="value"> 摘要摘要摘要摘要摘要摘要摘要摘要摘要摘要摘要摘要摘要摘要 </text>
+          </view>
+          <view class="row">
+            <view class="label">标签：</view>
+            <view class="value tabs">
+              <text selectable="true" class="tab" v-for="item in 3" :key="item">标签名</text>
+            </view>
+          </view>
+        </view>
+      </scroll-view>
+    </view>
+  </uni-popup>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const infoPopup = ref(null)
+// 打开信息弹出层
+const checkDetail = () => {
+  infoPopup.value.open()
+}
+// 关闭信息弹出层
+const closePopup = () => {
+  infoPopup.value.close()
+}
+</script>
 
 <style scoped lang="scss">
 .wallpaperInfo {
@@ -41,6 +100,83 @@
     .text {
       font-size: 26rpx;
       color: $text-font-color-2;
+    }
+  }
+}
+
+.info-popup {
+  padding: 30rpx;
+  background-color: #fff;
+  border-radius: 30rpx 30rpx 0 0;
+  overflow: hidden;
+
+  .pop-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .title {
+      color: $text-font-color-2;
+      font-size: 26rpx;
+    }
+
+    .close {
+      padding: 6rpx;
+    }
+  }
+
+  scroll-view {
+    max-height: 60vh;
+
+    .content {
+      .row {
+        display: flex;
+        padding: 16rpx 0;
+        font-size: 32rpx;
+        line-height: 1.7em;
+
+        .label {
+          width: 140rpx;
+          font-size: 30rpx;
+          color: $text-font-color-3;
+          text-align: right;
+        }
+
+        .value {
+          flex: 1;
+          width: 0;
+        }
+
+        .rate-box {
+          display: flex;
+          align-items: center;
+
+          .score {
+            padding-left: 10rpx;
+            font-size: 26rpx;
+            color: $text-font-color-2;
+          }
+        }
+
+        .tabs {
+          display: flex;
+          flex-wrap: nowrap;
+
+          .tab {
+            margin: 0 10rpx 0 10rpx;
+            border: 1px solid $brand-theme-color;
+            color: $brand-theme-color;
+            font-size: 22rpx;
+            padding: 10rpx 30rpx;
+            border-radius: 40rpx;
+            line-height: 1em;
+          }
+        }
+
+        .category {
+          color: $brand-theme-color;
+        }
+      }
     }
   }
 }
--- unpackage/dist/dev/mp-weixin/pages/preview/components/wallpaper-info.js
+++ unpackage/dist/dev/mp-weixin/pages/preview/components/wallpaper-info.js
@@ -1,29 +1,68 @@
 "use strict";
 const common_vendor = require("../../../common/vendor.js");
-const _sfc_main = {};
 if (!Array) {
   const _easycom_uni_icons2 = common_vendor.resolveComponent("uni-icons");
-  _easycom_uni_icons2();
+  const _easycom_uni_rate2 = common_vendor.resolveComponent("uni-rate");
+  const _easycom_uni_popup2 = common_vendor.resolveComponent("uni-popup");
+  (_easycom_uni_icons2 + _easycom_uni_rate2 + _easycom_uni_popup2)();
 }
 const _easycom_uni_icons = () => "../../../uni_modules/uni-icons/components/uni-icons/uni-icons.js";
+const _easycom_uni_rate = () => "../../../uni_modules/uni-rate/components/uni-rate/uni-rate.js";
+const _easycom_uni_popup = () => "../../../uni_modules/uni-popup/components/uni-popup/uni-popup.js";
 if (!Math) {
-  _easycom_uni_icons();
+  (_easycom_uni_icons + _easycom_uni_rate + _easycom_uni_popup)();
 }
-function _sfc_render(_ctx, _cache) {
-  return {
-    a: common_vendor.p({
-      type: "info",
-      size: "24"
-    }),
-    b: common_vendor.p({
-      type: "star",
-      size: "24"
-    }),
-    c: common_vendor.p({
-      type: "download",
-      size: "24"
-    })
-  };
-}
-const Component = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-5acf9712"]]);
+const _sfc_main = {
+  __name: "wallpaper-info",
+  setup(__props) {
+    const infoPopup = common_vendor.ref(null);
+    const checkDetail = () => {
+      infoPopup.value.open();
+    };
+    const closePopup = () => {
+      infoPopup.value.close();
+    };
+    return (_ctx, _cache) => {
+      return {
+        a: common_vendor.p({
+          type: "info",
+          size: "24"
+        }),
+        b: common_vendor.o(checkDetail),
+        c: common_vendor.p({
+          type: "star",
+          size: "24"
+        }),
+        d: common_vendor.p({
+          type: "download",
+          size: "24"
+        }),
+        e: common_vendor.o(closePopup),
+        f: common_vendor.p({
+          type: "closeempty",
+          size: "18",
+          color: "#999"
+        }),
+        g: common_vendor.p({
+          readonly: true,
+          touchable: true,
+          value: "3",
+          size: "16"
+        }),
+        h: common_vendor.f(3, (item, k0, i0) => {
+          return {
+            a: item
+          };
+        }),
+        i: common_vendor.sr(infoPopup, "5acf9712-3", {
+          "k": "infoPopup"
+        }),
+        j: common_vendor.p({
+          type: "bottom"
+        })
+      };
+    };
+  }
+};
+const Component = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__scopeId", "data-v-5acf9712"]]);
 wx.createComponent(Component);
